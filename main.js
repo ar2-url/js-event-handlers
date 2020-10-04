ad90728bca426c77827a6b71d2443538d18b3fa8
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-10-04 14:54:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+// The keys and notes variables store the piano keys
+const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
+const notes = [];
+keys.forEach(function(key){
+  notes.push(document.getElementById(key));
+})
+
+// Write named functions that change the color of the keys below
+const keyPlay = function(event) {
+  event.target.style.backgroundColor = '#6df0c2'; 
+  event.target.onkeypress
+}
+
+const keyReturn = function() {
+  event.target.style.backgroundColor = '';
+}
+// Write a named function with event handler properties
+let eventAssignment = function(note) {
+note.onmousedown = function() {
+  keyPlay(event);
+}
+note.onmouseup = function() {
+  keyReturn(event);
+  }
+}
+// Write a loop that runs the array elements through the function
+notes.forEach(eventAssignement)
+
+// These variables store the buttons that progress the user through the lyrics
+let nextOne = document.getElementById('first-next-line');
+let nextTwo = document.getElementById('second-next-line');
+let nextThree = document.getElementById('third-next-line');
+let startOver = document.getElementById('fourth-next-line');
+
+// This variable stores the '-END' lyric element
+let lastLyric = document.getElementById('column-optional');
+
+// These statements are "hiding" all the progress buttons, but the first one
+nextTwo.hidden = true;
+nextThree.hidden = true;
+startOver.hidden= true;
+
+// Write anonymous event handler property and function for the first progress button
+nextOne = function() {
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+
+  document.getElementById('letter-note-five') = 'D';
+  document.getElementById('letter-note-six') = 'C';
+}
+
+// Write anonymous event handler property and function for the second progress button
+nextTwo = function() {
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
+
+  document.getElementById('word-five') = 'DEAR';
+  document.getElementById('word-six') = 'FRI-';
+
+  document.getElementById('letter-note-three') = 'G';
+  document.getElementById('letter-note-four') = 'E';
+  document.getElementById('letter-note-five') = 'C';
+  document.getElementById('letter-note-six') = 'B';
+
+  nextTwo.lastLyric.display = 'inline-block';
+}
+
+// Write anonymous event handler property and function for the third progress button
+nextThree = function() {
+  startOver.hidden = false;
+  nextThree.hidden = true;
+
+  document.getElementById('word-one') = 'HAP-';
+  document.getElementById('word-two') = 'PY';
+  document.getElementById('word-three') = 'BIRTH';
+  document.getElementById('word-four') = 'DAY';
+  document.getElementById('word-five') = 'TO';
+  document.getElementById('word-six') = 'YOU!';
+
+  document.getElementById('letter-note-one') = 'F';
+  document.getElementById('letter-note-two') = 'F';
+  document.getElementById('letter-note-three') = 'E';
+  document.getElementById('letter-note-four') = 'C';
+  document.getElementById('letter-note-five') = 'D';
+  document.getElementById('letter-note-six') = 'C';
+  
+  lastLyric.style.display = 'none';
+}
+// This is the event handler property and function for the startOver button
+startOver.onclick = function() {
+  nextOne.hidden = false;
+  startOver.hidden = true;
+   document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('letter-note-one').innerHTML = 'G';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('letter-note-two').innerHTML = 'G';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('letter-note-three').innerHTML = 'A';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('letter-note-four').innerHTML = 'G';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('word-six').innerHTML = 'YOU!';
+  document.getElementById('letter-note-six').innerHTML = 'B';
+}
+
